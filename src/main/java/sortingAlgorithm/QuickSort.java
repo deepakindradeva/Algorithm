0b620db1210721ef50a96121a2a0590a5757da6d
deepakindradeva <deepakindradeva@gmail.com>
2022-01-29 01:38:16 +0530

    Code Clean UP
    Added Random Pivot Selector
    Overloaded QuickSort for cleaner code

--- src/main/java/sortingAlgorithm/QuickSort.java
+++ src/main/java/sortingAlgorithm/QuickSort.java
@@ -1,20 +1,29 @@
 package sortingAlgorithm;
 
+import java.util.Random;
+
 public class QuickSort {
     public static void main(String[] args) {
         int[] array = new int[] {1, 8, 3, 9, 4, 5, 7};
-        quickSort(array, 0, array.length - 1);
+        quickSort(array);
 
         for (int j : array) {
             System.out.println(j);
         }
     }
 
+    private static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
     private static void quickSort(int[] array, int lowIndex, int highIndex) {
         if(lowIndex >= highIndex){
             return;
         }
-        int pivot = array[highIndex];
+
+        // Randomize the Pivot selection
+        int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+        int pivot = array[pivotIndex];
         int leftPointer = getLeftPointer(array, lowIndex, highIndex, pivot);
         quickSort(array, lowIndex, leftPointer - 1);
         quickSort(array, leftPointer + 1, highIndex);
